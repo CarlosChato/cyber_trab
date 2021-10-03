e6f06879dd0a9e4429eb1b747f314337a6c0411e
CarlosChato <carlosyalgo20@gmail.com>
2021-10-03 20:46:35 +0200

    added padding

--- main.py
+++ main.py
@@ -5,15 +5,12 @@ class MyApp():
         self.ventana = tk.Tk()
         self.ventana.geometry("600x600")
         self.ventana.configure(bg="grey")
-        #space = tkinter.Label(height=5, bg="grey",)
-        #space2 = tkinter.Label(height=5,bg="grey")
+        
         login_butt = tk.Button(self.ventana, text="Login", width=20, height=2)
         sign_up_butt = tk.Button(self.ventana, text = "Sign Up", width=20, height=2)
 
-        #space.pack()
-        login_butt.pack(pady=100)
-        #space2.pack()
-        sign_up_butt.pack(pady=10)
+        login_butt.pack(pady=(200,10)) #padding 200px for top and 10 px for bot
+        sign_up_butt.pack(pady=(10,200)) #padding like the last one, but inverse
 
 app = MyApp()
 app.ventana.mainloop()  
